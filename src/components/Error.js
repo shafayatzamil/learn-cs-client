feaee39069ef0c3b86a3421ee2f8fda35dcfff34
shafayat zamil <zamilkhan111.za@gmail.com>
2022-10-27 23:26:11 +0600

    Error page changed

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import ErrorPage from '../assets/Error page.png'
 
 //Error page  set up
 const Error = () => {
    return (
       <div className='h-screen flex items-center justify-center'>
-         <h1 className='text-red-500 text-center text-4xl font-bold tracking-wider'>404 | page not found</h1>
+         <div><h1 className='text-red-500 text-center text-4xl font-bold tracking-wider'>404 | page not found</h1>
+         <img src={ErrorPage} alt="" srcset="" /></div>
       </div>
    )
 }
