46b8a5b3a1785bfcc7e238977dc020d5dc325498
shafayat zamil <zamilkhan111.za@gmail.com>
2022-10-27 22:29:17 +0600

    set error set up

--- src/components/Register.js
+++ src/components/Register.js
@@ -36,6 +36,7 @@ const Register = () => {
          })
          .then((error) => {
             console.error(error);
+            setSignupError(error.message);
          });
    }
 
@@ -47,6 +48,7 @@ const Register = () => {
          })
          .catch((error) => {
             console.error(error);
+            setSignupError(error.message);
          })
    }
 
@@ -59,7 +61,8 @@ const Register = () => {
             <input type='email' name='email' placeholder='Email' className='px-5 py-4 border-primary border-br-1' />
             <input type='password' name='password' placeholder='Password' className='px-5 py-4 mb-6 border-primary border-br-1' />
             <button className='self-start px-6 py-3 text-lg bg-primary text-white rounded'>Sign up</button>
-            {signupError && <span>{signupError}</span>}
+            {signupError && <span className='text-red-600/75'>{signupError}</span>}
+            {/* <p className='text-red-600/75'>{loginError}</p> */}
          </form>
 
          <div className='flex items-center flex-wrap gap-4'>
@@ -91,6 +94,7 @@ const Register = () => {
                   >
                      <path d='M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z'></path>
                   </svg>
+
                </button>
             </div>
          </div>
